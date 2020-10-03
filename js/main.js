cea335cfe64e36c234bcd416c7f60c0f88fcb59a
Manuel Thibault <tmanuel@student.42.fr>
2020-10-03 22:33:30 +0200

    Add GrandPiano gltf model, draco and lights

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 //import { OrbitControls } from './three/OrbitControls.js';
 
-var camera, controls, scene, renderer, pianoKeys, player, futurBoxs = [];
+var camera, controls, scene, renderer, pianoKeys, player, futurBoxs = [], pianoFloor = 20;
 
 init();
 //render(); // remove when using next line for animation loop (requestAnimationFrame)
@@ -29,17 +29,19 @@ function generatePiano(scene = undefined, opacity = 1) {
             line = new THREE.LineSegments(whiteEdges, new THREE.LineBasicMaterial({ color: color }));
             box.position.x = n - modificator;
             line.position.x = n - modificator;
+            box.position.y = pianoFloor;
+            line.position.y = pianoFloor;
 
         }
         else {
             box = new THREE.Mesh(blackGeometry, blackMaterial);
-            color = 0xffffff;
+            color = 0x666666;
             line = new THREE.LineSegments(blackEdges, new THREE.LineBasicMaterial({ color: color }));
             box.position.x = n - modificator - 0.5;
-            box.position.y = 0.4;
+            box.position.y = 0.4 + pianoFloor;
             box.position.z = -1;
             line.position.x = n - modificator - 0.5;
-            line.position.y = 0.4;
+            line.position.y = 0.4 + pianoFloor;
             line.position.z = -1;
             modificator += 1;
         }
@@ -54,15 +56,15 @@ function generatePiano(scene = undefined, opacity = 1) {
 
 function init() {
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x555555);
+    scene.background = new THREE.Color(0xcce0ff);
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(0, 20, 35);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(0, 100, 100);
 
     // controls
 
@@ -79,10 +81,70 @@ function init() {
     controls.maxDistance = 500;
 
     controls.maxPolarAngle = Math.PI / 2;
+    // ground 
+    var loader = new THREE.TextureLoader();
+    var groundTexture = loader.load('./images/ground.jpg');
+    groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+    groundTexture.repeat.set(10, 10);
+    groundTexture.anisotropy = 16;
+    groundTexture.encoding = THREE.sRGBEncoding;
 
-    // world
+    var groundMaterial = new THREE.MeshLambertMaterial({ map: groundTexture });
+
+    var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(1000, 1000), groundMaterial);
+    mesh.position.y = -10;
+    mesh.rotation.x = - Math.PI / 2;
+    mesh.receiveShadow = true;
+    scene.add(mesh);
+    // piano Keyboard
 
     pianoKeys = generatePiano(scene);
+
+    // piano model
+    var dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('./js/draco/gltf/');
+
+    var loader = new GLTFLoader();
+
+    loader.setDRACOLoader(dracoLoader);
+    loader.load('./js/model/Grand_Piano_test.glb', function (gltf) {
+
+        var model = gltf.scene;
+        model.position.set(43, -30 + pianoFloor, -3);
+        model.scale.set(37, 37, 37);
+
+        scene.add(model);
+
+        animate();
+
+    }, undefined, function (e) {
+
+        console.error(e);
+
+    });
+
+    // lights
+    var ambient = new THREE.AmbientLight(0xffffff, 1);
+    var sphere = new THREE.SphereBufferGeometry(0.5, 16, 8);
+    let light1 = new THREE.PointLight(0xffffff, 150, 35);
+    light1.position.set(0, 20, 20)
+    scene.add(light1);
+
+    let light2 = new THREE.PointLight(0xffffff, 2, 150);
+    light2.position.set(35, 25, -70)
+    scene.add(light2);
+
+    let light3 = new THREE.PointLight(0xffffff, 2, 150);
+    light3.position.set(-55, 25, -60)
+    scene.add(light3);
+
+    let light4 = new THREE.PointLight(0xffffff, 2, 150);
+    light4.position.set(0, 45, -150)
+    scene.add(light4);
+    scene.add(ambient);
+
+
+
 
     window.addEventListener('resize', onWindowResize, true);
 
@@ -97,10 +159,29 @@ function onWindowResize() {
 
 }
 
+let t1 = Date.now();
+let previouscurrentTime = -1;
+let currentTime;
 function animate() {
 
     requestAnimationFrame(animate);
 
+    if (player != undefined) {
+        currentTime = player.currentTime;
+        if (!MIDI.Player.playing)
+            currentTime = previouscurrentTime;
+        if (previouscurrentTime == player.currentTime && MIDI.Player.playing) {
+            currentTime = Date.now() - t1 + player.currentTime;
+        }
+        else {
+            t1 = Date.now();
+            previouscurrentTime = currentTime;
+        }
+    }
+    for (let box of futurBoxs) {
+        box.box.position.y = box.baseY - currentTime / 100;
+        box.line.position.y = box.baseY - currentTime / 100;
+    }
     controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
 
     render();
@@ -118,32 +199,38 @@ let colorNote_w = [0x42d4f5, 0xe33030];
 let colorNote_b = [0x10b5b2, 0xbf2828];
 let createBox = function (pianoKey, width, data) {
     let color = colorNote_b[data.track % 2];
-    if (pianoKey.isWhite) 
+    if (pianoKey.isWhite)
         color = colorNote_w[data.track % 2];
     const velocity = data.velocity * 10;
     const datatime = data.time * 10;
     const geometry = new THREE.BoxGeometry(width, velocity, 1);
     const material = new THREE.MeshBasicMaterial({ color: color, transparent: true, opacity: 1, side: THREE.DoubleSide });
     const edges = new THREE.EdgesGeometry(geometry);
-    
+
     let box = new THREE.Mesh(geometry, material);
     let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 }));
 
     box.position.y = datatime + velocity / 2;
     box.position.x = pianoKey.box.position.x + 0.1;
-    line.position.y = datatime + velocity/ 2;
+    line.position.y = datatime + velocity / 2;
     line.position.x = pianoKey.box.position.x + 0.1;
     if (pianoKey.isWhite) {
         box.position.z = 2;
         line.position.z = 2;
     }
+    box.castShadow = true;
     scene.add(line)
     scene.add(box)
-    futurBoxs.push(box);
-    futurBoxs.push(line);
+    futurBoxs.push({ box: box, line: line, baseY: datatime + velocity / 2 + pianoFloor });
 }
 
 let createFuturBox = function () {
+    //clear box
+    for (box of futurBoxs) {
+        scene.remove(box.box);
+        scene.remove(box.line);
+    }
+    futurBoxs = [];
     for (data of mididata) {
         if (data.msg.subtype == "noteOn") {
             let n = data.msg.noteNumber - 21;
@@ -161,9 +248,10 @@ let createFuturBox = function () {
     }
 };
 
-let mididata = [];
+let mididata;
 let addTimecode = function () {
     let t = 0;
+    mididata = [];
     for (let data of player.data) {
         t += data[1];
         let newdata = {
@@ -184,6 +272,7 @@ let addTimecode = function () {
             }
         }
     }
+    createFuturBox();
 }
 
 
@@ -271,10 +360,8 @@ eventjs.add(window, "load", function (event) {
             MIDI.setVolume(0, 20);
             player.loadFile(song[songid % song.length]);
             addTimecode();
-            createFuturBox();
             player.addListener(function (data) {
                 let pianoKey = data.note - 21;
-                console.log(pianoKeys[pianoKey]);
                 if (data.message === 144) {
                     setKey(pianoKey, true);
                 }
@@ -327,8 +414,6 @@ let MIDIPlayerPercentage = function (player) {
         if (now === end) { // go to next song
             let id = ++songid % song.length;
             player.loadFile(song[id], player.start); // load MIDI
-            let songnamediv = document.getElementById("songname");
-            songnamediv.innerHTML = songname[songid % songname.length];
             addTimecode();
         }
         // display the information to the user
